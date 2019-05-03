982142063090d157a01c7d897830657ffbaf67a6
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-05-03 13:32:47 -0400

    small fix

--- includes/Module/Hooks/ClicShoppingAdmin/Dashboard/IndexDashboardTopProductExpected.php
+++ includes/Module/Hooks/ClicShoppingAdmin/Dashboard/IndexDashboardTopProductExpected.php
@@ -31,25 +31,25 @@
       $Qproducts = $this->db->prepare('select count(*) as count from :table_products where products_date_available <> null');
       $Qproducts->execute();
 
-      $number_products_expected = $Qproducts->valueInt('products_date_available');
+      $number_products_expected = $Qproducts->valueInt('count');
 
-      if ($number_products_expected > 0) {
+      if ($number_products_expected != 0) {
         $text = CLICSHOPPING::getDef('text_number_products_expected');
         $text_view = CLICSHOPPING::getDef('text_view');
 
         $output = '
 <div style="padding-right:0.5rem; padding-top:0.5rem">
-    <div class="card bg-primary">
-      <div class="card-body">
-        <div class="row">
-          <h5 class="card-title text-white"><i class="fas fas fa-info"  aria-hidden="true"></i> ' . $text . '</h5>
-        </div>
-        <div class="col-md-12">
-          <span h5 class="text-white">' . $number_products_expected . '</span>
-          <span ><small class="text-white">' . HTML::link(CLICSHOPPING::link(null, 'A&Report\StatsProductsExpected&StatsProductsExpected'), $text_view, 'class="text-white"') . '</small></span>
-        </div>
+  <div class="card bg-primary">
+    <div class="card-body">
+      <div class="row">
+        <h5 class="card-title text-white"><i class="fas fas fa-info"  aria-hidden="true"></i> ' . $text . '</h5>
+      </div>
+      <div class="col-md-12">
+        <span h5 class="text-white">' . $number_products_expected . '</span>
+        <span ><small class="text-white">' . HTML::link(CLICSHOPPING::link(null, 'A&Report\StatsProductsExpected&StatsProductsExpected'), $text_view, 'class="text-white"') . '</small></span>
       </div>
     </div>
+  </div>
 </div>
 ';
 
